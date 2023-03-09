386a40e8c22bc98f1770d888beac5e66089580cc
jsadler2 <jeff.sadler@okstate.edu>
2023-03-09 11:32:10 -0600

    py fxn to adj catch attr in zarr files

---
+++ 2a_model/out/adjust_catch_attr.py
@@ -0,0 +1,33 @@
+import xarray as xr
+import numpy as np
+
+ds = xr.open_zarr("well_obs_io.zarr")
+
+test_sites = ['01475530', '01475548']
+
+trn_sites = ds.site_id[~np.isin(ds.site_id, test_sites)]
+
+ds_trn = ds.sel(site_id = trn_sites)
+
+vars_in_question = ['TOT_BASIN_SLOPE', 'TOT_IMPV11']
+
+
+for v in vars_in_question:
+    ds_adj = ds.copy(deep=True)
+    mean = ds_trn[v].mean().values
+    for da in ds_adj.values():
+        if da.name == v:
+            da.loc[dict(site_id = test_sites)] = mean
+    for var in ds_adj.variables:
+        ds_adj[var].encoding.clear()
+    ds_adj.to_zarr(f"well_obs_io_ADJ_{v}.zarr")
+
+for v in vars_in_question:
+    mean = ds_trn[v].mean().values
+    for da in ds.values():
+        if da.name == v:
+            da.loc[dict(site_id = test_sites)] = mean
+
+for var in ds.variables:
+    ds[var].encoding.clear()
+ds.to_zarr(f"well_obs_io_ADJ_IMP_and_SLP.zarr")
